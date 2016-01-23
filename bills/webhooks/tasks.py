26727bf272a49b294da278b5e719888f65a0a068
Dan Palmer <dan@danpalmer.me>
2016-01-23 15:50:02 +0000

    Initial webhook handling

--- bills/webhooks/tasks.py
+++ bills/webhooks/tasks.py
@@ -2,3 +2,12 @@ from bills import mondo
 from bills.celery import queue
 from bills.accounts.models import Account
 
+def receive_transaction_hook(account, transaction):
+    receive_transaction_hook_task.delay(account.pk, transaction)
+
+@queue.task
+def receive_transaction_hook_task(account_id, transaction):
+    account = Account.objects.get(pk=account_id)
+
+    print(transaction)
+    print(account)
--- bills/webhooks/views.py
+++ bills/webhooks/views.py
@@ -1,13 +1,20 @@
+import json
+
 from django.http import HttpResponse
 from django.views.generic import DetailView
 from django.db.transaction import on_commit
 
 from bills.accounts.models import Account
 
+from .tasks import receive_transaction_hook
+
 class Hook(DetailView):
     model = Account
 
     def post(self, request, pk):
         account = self.get_object()
-        # TODO: receive the webhook
+
+        tx_json = json.loads(request.body.decode('utf8'))
+        on_commit(lambda: receive_transaction_hook(account, tx_json))
+
         return HttpResponse()
