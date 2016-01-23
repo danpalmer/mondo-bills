77524e6337c920569f2cd9230ce75da4db52358d
Thomas Grainger <tom.grainger@procensus.com>
2016-01-23 18:23:50 +0000

    merchants can be null

--- bills/transactions/tasks.py
+++ bills/transactions/tasks.py
@@ -6,8 +6,10 @@ from bills.accounts.models import Account
 from bills.transactions.models import MerchantGroup
 from bills import mondo
 
+from . import utils
+
 def download_all_transactions(account):
-    receive_transaction_hook_task.delay(account.pk)
+    download_all_transactions_task.delay(account.pk)
 
 @queue.task
 def download_all_transactions_task(account_id):
@@ -17,23 +19,4 @@ def download_all_transactions_task(account_id):
     transactions = mondo.get_all_transactions(user, account.mondo_account_id)
 
     for data in transactions:
-        merchant, _ = MerchantGroup.objects.get_or_create(
-            mondo_group_id=data['merchant']['group_id'],
-            defaults={
-                'name': data['merchant']['name'],
-                'logo_url': data['merchant']['logo'],
-                'category': data['merchant']['category'],
-            },
-        )
-
-        account.transactions.get_or_create(
-            mondo_transaction_id=data['id'],
-            defaults={
-                'description': data['description'],
-                'amount': data['amount'],
-                'is_load': data['is_load'],
-                'category': data['category'],
-                'mondo_created': dateparser.parse(data['created']),
-                'merchant_group': merchant,
-            },
-        )
+        utils.store_transaction(account, data)
--- bills/webhooks/tasks.py
+++ bills/webhooks/tasks.py
@@ -1,8 +1,6 @@
-from dateutil import parser as dateparser
-
 from bills.celery import queue
 from bills.accounts.models import Account
-from bills.transactions.models import MerchantGroup
+from bills.transactions import utils as transaction_utils
 
 def receive_transaction_hook(account, transaction):
     receive_transaction_hook_task.delay(account.pk, transaction)
@@ -15,22 +13,4 @@ def receive_transaction_hook_task(account_id, transaction):
         return
 
     data = transaction['data']
-
-    merchant, _ = MerchantGroup.objects.get_or_create(
-        mondo_group_id=data['merchant']['group_id'],
-        defaults={
-            'name': data['merchant']['name'],
-            'logo_url': data['merchant']['logo'],
-            'category': data['merchant']['category'],
-        },
-    )
-
-    account.transactions.create(
-        mondo_transaction_id=data['id'],
-        description=data['description'],
-        amount=data['amount'],
-        is_load=data['is_load'],
-        category=data['category'],
-        mondo_created=dateparser.parse(data['created']),
-        merchant_group=merchant,
-    )
+    transaction_utils.store_transaction(account, data)
