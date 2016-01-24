02bdce5e2acbebaa07cdb8f0bcc0b997f03cbf3a
Dan Palmer <dan@danpalmer.me>
2016-01-24 13:17:14 +0000

    Must be login_required here

--- bills/dashboard/urls.py
+++ bills/dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
+
 urlpatterns = [
-    url(r'^dashboard$', views.Dashboard.as_view(), name='view'),
+    url(r'^dashboard$', login_required(views.Dashboard.as_view()), name='view'),
 ]
