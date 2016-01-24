f0e02540273b075a29f86ddbc81383963fa99e73
Dan Palmer <dan@danpalmer.me>
2016-01-24 12:29:29 +0000

    List recurring transactions

--- bills/dashboard/views.py
+++ bills/dashboard/views.py
@@ -1,27 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-from bills.transactions import utils
+from bills.recurring.models import RecurringTransaction
 
 
-class Dashboard(TemplateView):
+class Dashboard(ListView):
     template_name = 'dashboard/view.html'
+    context_object_name = 'recurring_transactions'
 
-
-    def get_context_data(self, *args, **kwargs):
-        ctx = super().get_context_data(*args, **kwargs)
-        to_return = {}
-        to_return.update(ctx)
-        user = self.request.user
-
-        accounts = []
-        for account in user.accounts.all():
-            accounts.append({
-                'actual_account': account,
-                'recurring_transactions': utils.get_recurring_merchants(account),
-            })
-        to_return.update({
-            'user': self.request.user,
-            'accounts': accounts,
-        })
-
-        return to_return
+    def get_queryset(self):
+        return RecurringTransaction.objects.filter(
+            account__user=self.request.user,
+        )
